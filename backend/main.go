56d3b262316b24094573a140906b891901bd54b5
Xuesen Cui <xcui83@gatech.edu>
2023-10-18 20:27:14 +0800

    added main code

--- backend/main.go
+++ backend/main.go
@@ -16,9 +16,9 @@ func main() {
 	// Define a handler for the '/temperature/set' route
 	http.HandleFunc("/temperature/set", helloHandler)
 
-	// Start the HTTP server on port 1234
-	fmt.Println("Server is listening on :1234...")
-	err := http.ListenAndServe(":1234", nil)
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
