484d1def188a21f5343722259eb42c37ddc23ed1
Xuesen Cui <xcui83@gatech.edu>
2023-11-10 21:05:39 +0800

    added backend http api docsclear

--- backend/application/sensor/switch.go
+++ backend/application/sensor/switch.go
@@ -45,65 +45,45 @@ func createAlert(app *application.App, roomID uint32, acID, switchID uint32) err
 	if err != nil {
 		return err
 	}
-	return sendEmail2(roomID, acID, switchID)
+	return sendAlertEmail(getAlertEmailBody(roomID, acID, switchID))
 }
 
-// Default receiver list
-var receivers = []string{
-	"xcui83@gatech.edu",
-}
-
-func sendEmail(roomID, acID, switchID uint32) error {
-	senderEmail := "CS6300LocalProject@outlook.com"
-	senderPassword := "6300GOGOGO"
-	smtpServer := "smtp.office365.com"
-	smtpPort := 587
-	emailSubject := "GTSI Running AC and Open Windows Alert"
-	emailBody := fmt.Sprintf("Room ID: %d\nAC ID: %d\nSwitch ID: %d\nPlease go check out", roomID, acID, switchID)
-	message := "Subject: " + emailSubject + "\n" + emailBody
-	auth := smtp.PlainAuth("", senderEmail, senderPassword, smtpServer)
-	err := smtp.SendMail(fmt.Sprintf("%s:%d", smtpServer, smtpPort), auth, senderEmail, receivers, []byte(message))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func sendEmail2(roomID, acID, switchID uint32) error {
+func sendAlertEmail(emailBody string) error {
 	m := gomail.NewMessage()
 	m.SetHeader("From", "CS6300LocalProject@outlook.com")
 	m.SetHeader("To", "xcui83@gatech.edu")
 	m.SetHeader("Subject", "Urgent: Action Required - Running AC and Open Windows Detected")
-
-	emailBody := fmt.Sprintf(`Dear Operation Team,
-
-We have detected a critical issue in your assigned area that requires immediate attention.
-
-Alert: GTSI Running AC and Open Windows
-
-Our monitoring systems have identified that the Air Conditioning (AC) is running while windows are open in the following location:
-
-- Room ID: %d
-- AC ID: %d
-- Switch ID: %d
-
-This situation leads to energy inefficiency and compromises the environmental control protocols of the facility.
-
-Immediate Action Required:
-1. Please visit the room urgently to address this issue.
-2. Close all open windows in the room.
-3. Ensure the AC settings are adjusted appropriately or turned off if not needed.
-
-This alert aims to maintain optimal energy usage and environmental standards within our facilities. Your prompt response to resolve this matter is highly appreciated.
-
-Thank you for your immediate attention to this issue.
-
-Best regards,
-
-CS6300 Local Project Team`, roomID, acID, switchID)
-
 	m.SetBody("text/plain", emailBody)
 	d := gomail.NewDialer("smtp.office365.com", 587, "CS6300LocalProject@outlook.com", "6300GOGOGO")
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	return d.DialAndSend(m)
+}
+
+func getAlertEmailBody(roomID, acID, switchID uint32) string {
+	return fmt.Sprintf(`Dear Operation Team,
+
+	We have detected a critical issue in your assigned area that requires immediate attention.
+	
+	Alert: GTSI Running AC and Open Windows
+	
+	Our monitoring systems have identified that the Air Conditioning (AC) is running while windows are open in the following location:
+	
+	- Room ID: %d
+	- AC ID: %d
+	- Switch ID: %d
+	
+	This situation leads to energy inefficiency and compromises the environmental control protocols of the facility.
+	
+	Immediate Action Required:
+	1. Please visit the room urgently to address this issue.
+	2. Close all open windows in the room.
+	3. Ensure the AC settings are adjusted appropriately or turned off if not needed.
+	
+	This alert aims to maintain optimal energy usage and environmental standards within our facilities. Your prompt response to resolve this matter is highly appreciated.
+	
+	Thank you for your immediate attention to this issue.
+	
+	Best regards,
+	
+	CS6300 Local Project Team`, roomID, acID, switchID)
 }
