95b2c561717c448a3d42599efa2759241bef23c3
xwang3234 <xwang3234@gatech.edu>
2023-11-16 11:34:19 +0800

    added energy saving page

--- frontend/gtsi-main-CS6300/miniprogram/pages/energySaving/index.js
+++ frontend/gtsi-main-CS6300/miniprogram/pages/energySaving/index.js
@@ -5,14 +5,54 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+      alerts: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
+      this.fetchAlerts();
+    },
+    fetchAlerts: function() {
+      const that = this;
+      wx.request({
+        url: 'http://175.178.194.182:8080/alert/list',
+        method: 'GET',
+        success(res) {
+          if (res.statusCode === 200) {
+            that.processAlerts(res.data.alerts);
+          }
+        },
+        fail() {
+          wx.showToast({
+            title: 'Failed to load alerts',
+            icon: 'none'
+          });
+        }
+      });
+    },
 
+    processAlerts: function(alerts) {
+      const processedAlerts = alerts.map(alert => {
+        return {
+          ...alert,
+          alert_type_text: this.getAlertTypeText(alert.alert_type),
+          status_text: alert.status === 0 ? 'Open' : 'Closed'
+        };
+      });
+  
+      this.setData({
+        alerts: processedAlerts
+      });
+    },
+  
+    getAlertTypeText: function(type) {
+      switch (type) {
+        case 0: return 'AlertRunningAC';
+        case 1: return 'AlertOpenWindow';
+        default: return 'Unknown';
+      }
     },
 
     /**
